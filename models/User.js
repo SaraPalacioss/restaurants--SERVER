41f0ce26c3a401387f5b1b5536011b6cee99cf2e
sarastackhouse <sarapalacios7@gmail.com>
2021-07-13 20:09:53 +0200

    User model created

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose  = require('mongoose');
+const Schema    = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength: 8 }
+}, {
+  timestamps: true,
+  toJSON: {
+    transform: (doc, ret) => {
+      ret.id = doc._id;
+      delete ret._id;
+      delete ret.__v;
+      delete ret.password;
+      delete ret.createdAt;
+      return ret;
+    }
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
+
