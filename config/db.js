56099102c2c3567fa313b6700c80efd7d4058446
sarastackhouse <sarapalacios7@gmail.com>
2021-07-13 20:12:35 +0200

    Database config done

---
+++ config/db.js
@@ -0,0 +1,25 @@
+const mongoose  = require('mongoose');
+
+const connectDB = async () => {
+
+    try {
+        await 
+        mongoose.connect(process.env.DB, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        })
+        mongoose.set('useCreateIndex', true);
+        ;
+        console.log('DB Connected');
+
+    } catch (error) {
+        console.log('There was an error with conexion')
+        console.log(error);
+        process.exit(1); 
+    }
+
+};
+
+
+module.exports = connectDB;
