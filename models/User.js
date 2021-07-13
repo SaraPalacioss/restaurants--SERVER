fb6c5a5359bcfaddfad9445a3d267020bcc147ce
sarastackhouse <sarapalacios7@gmail.com>
2021-07-14 00:57:36 +0200

    Favourites in user model created

--- models/User.js
+++ models/User.js
@@ -3,7 +3,8 @@ const Schema    = mongoose.Schema;
 
 const userSchema = new Schema({
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true, minlength: 8 }
+  password: { type: String, required: true, minlength: 8 },
+  favourites: { type: String }
 }, {
   timestamps: true,
   toJSON: {
